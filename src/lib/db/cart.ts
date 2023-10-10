438df58d65a84cc3c983ed0765d977356c1644eb
Felipe300 <caracoloro@gmail.com>
2023-10-10 20:45:41 -0300

    feat: handle auth.js sessions

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { prisma } from "./prisma"
 import { cookies } from "next/dist/client/components/headers"
-import { Prisma } from "@prisma/client"
+import { Cart, CartItem, Prisma } from "@prisma/client"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 // database operations used in different places
 // these are not server functions, just functions
@@ -18,17 +20,27 @@ export type ShoppingCart = CartWithProducts & {
 }
 
 export async function getCart (): Promise<ShoppingCart | null> {
+  const session = await getServerSession(authOptions)
 	const localCartId = cookies().get("localCartId")?.value
-	const cart = localCartId ?
-		await prisma.cart.findUnique({
-			where: { id: localCartId },
-			include: { items: { include: { product: true } } }
-		})
-		: null
+  let cart: CartWithProducts | null = null
 
-	if (!cart) {
-		return null
-	}
+  if(session) {
+    cart = await prisma.cart.findFirst({
+      where: { userId: session.user.id },
+      include: { items: { include: { product: true } } }
+    })
+  } else {
+    cart = localCartId ?
+      await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { items: { include: { product: true } } }
+      })
+      : null
+  }
+
+  if (!cart) {
+    return null
+  }
 
 	return {
 		...cart,
@@ -38,13 +50,21 @@ export async function getCart (): Promise<ShoppingCart | null> {
 }
 
 export async function createCart (): Promise<ShoppingCart> {
-	const newCart = await prisma.cart.create({
-		data: {}
-	})
+  const session = await getServerSession(authOptions)
+  let newCart: Cart
 
+  if(session) {
+    newCart = await prisma.cart.create({
+      data: { userId: session.user.id }
+    })
+  } else {
+    newCart = await prisma.cart.create({
+      data: {}
+    })
 	// newCart.id must be encrypted
 	// the cookies function also ask for other securities
-	cookies().set("localCartId", newCart.id)
+    cookies().set("localCartId", newCart.id)
+  }
 
   return {
     ...newCart,
@@ -53,3 +73,77 @@ export async function createCart (): Promise<ShoppingCart> {
     subtotal: 0
   }
 }
+
+export async function mergeAnonymusCartIntoUserCart (userId: string) {
+  const localCartId = cookies().get("localCartId")?.value
+
+  const localCart = localCartId
+    ? await prisma.cart.findUnique({
+      where: { id: localCartId },
+      include: { items: true }
+    })
+    : null
+
+  if(!localCart) return
+
+  const userCart = await prisma.cart.findFirst({
+    where: { userId },
+    include: { items : true }
+  })
+
+  await prisma.$transaction(async (tx) => {
+    if(userCart) {
+      const mergedCartItems = mergeCartItems(localCart.items, userCart.items)
+
+      await tx.cartItem.deleteMany({
+        where: { cartId: userCart.id }
+      })
+
+      await tx.cartItem.createMany({
+        data: mergedCartItems.map((item) => ({
+          cartId: userCart.id,
+          productId: item.productId,
+          quantity: item.quantity
+        }))
+      })
+    } else {
+      await tx.cart.create({
+        data: {
+          userId,
+          items: {
+            createMany: {
+              data: localCart.items.map((item) => ({
+                productId: item.productId,
+                quantity: item.quantity
+              }))
+            }
+          }
+        }
+      })
+    }
+
+    await tx.cart.delete({
+      where: {
+        id: localCart.id
+      }
+    })
+
+    // if error throws ("Transaction failed")
+    cookies().set("localCartId", "")
+  })
+}
+
+function mergeCartItems (...cartItems: CartItem[][]): CartItem[] {
+  return cartItems.reduce((acc, items) => {
+    items.forEach((item) => {
+      const existingItem = acc.find((i) => i.productId === item.productId)
+      if(existingItem) {
+        existingItem.quantity += item.quantity
+      } else {
+        acc.push(item)
+      }
+    })
+    return acc
+  }, [] as CartItem[])
+}
+
